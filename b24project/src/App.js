336c62f67f3d1721b00d0b5c603d8f08b050fe44
VaishnaviDhobale <masaieducation17@gmail.com>
2023-03-28 14:43:14 +0530

    1st commit

---
+++ b24project/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome guys, remember 1 thing we have to do best.</h1>
+    </div>
+  );
+}
+
+export default App;
