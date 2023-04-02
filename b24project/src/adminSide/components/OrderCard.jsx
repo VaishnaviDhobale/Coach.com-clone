1c2cad81546123bf5ceccc4f98290ff17a2c9e0e
rajeshyadav9931 <rajeshkumaryadav9931grd@gmail.com>
2023-04-02 23:03:15 +0530

    day-6

--- b24project/src/adminSide/components/OrderCard.jsx
+++ b24project/src/adminSide/components/OrderCard.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-const OrderCard = (props) => {
+const OrderCard = ({image,title,price,category,email,transaction_id}) => {
     console.log(props)
   return (
-    <div>OrderCard</div>
+    <div>
+       <h1>Email:{email}</h1>
+      <h2>Transaction Id:{transaction_id}</h2>
+      <h2>Title:{title}</h2>
+      <h2>Category:{category}</h2>
+      <h2>Price:{price}</h2>
+
+      <img src={image} alt={title} />
+    </div>
   )
 }
 
--- b24project/src/adminSide/components/UserCard.jsx
+++ b24project/src/adminSide/components/UserCard.jsx
@@ -1,9 +1,15 @@
 
 import React from 'react'
 
-const UserCard = () => {
+const UserCard = ({name,email,age,city,address,pincode}) => {
   return (
-    <div>UserCard</div>
+    <div><h1>Name:{name}</h1>
+    <h3>Email:-{email}</h3>
+    <h3>Age:{age}</h3>
+    <p>City:{city}</p>
+    <p>Address:-{address}</p>
+    <p>Pincode:-{pincode}</p>
+</div>
   )
 }
 
