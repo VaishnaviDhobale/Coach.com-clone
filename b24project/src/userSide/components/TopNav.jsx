2048015855dc9e88b1d08f916d687d972b0375d4
VaishnaviDhobale <masaieducation17@gmail.com>
2023-05-11 17:56:34 +0530

    navbar update

--- b24project/src/userSide/components/TopNav.jsx
+++ b24project/src/userSide/components/TopNav.jsx
@@ -1,46 +1,40 @@
 import "./Topnav.css";
-import {MdPlace} from "react-icons/md";
+import { MdPlace } from "react-icons/md";
 import { Text } from "@chakra-ui/react";
-import {RxHeart} from "react-icons/rx";
-import {IoPersonOutline,IoBagHandleOutline} from "react-icons/io5";
-import { Button } from "@chakra-ui/react";
+import { RxHeart } from "react-icons/rx";
+import { IoPersonOutline, IoBagHandleOutline } from "react-icons/io5";
+import { Box,Button } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import {MdSearch} from "react-icons/md"
+import { MdSearch } from "react-icons/md";
 
+const TopNavbar = () => {
+  return (
+    <Box className="topnav">
+      <Box>
+        <img src="https://i.postimg.cc/SsRfrTjB/IMG-20230402-WA0000.jpg" />
+      </Box>
 
-
-const TopNavbar=()=>{
-    return (
-        <div className="topnav">
-
-             
-        
-
-            <div>
-         
-            <img  src="https://i.postimg.cc/SsRfrTjB/IMG-20230402-WA0000.jpg"/>
-            </div>
-    
-           {/* <Text paddingTop="3"><RxHeart size={30} className="wshlst"/></Text>
+      {/* <Text paddingTop="3"><RxHeart size={30} className="wshlst"/></Text>
            <Text marginLeft="200"  paddingTop="3"><IoPersonOutline size={30} className="signup"/></Text>
            <Text marginLeft="500" paddingTop="3"><IoBagHandleOutline size={30} className="shopbag"/></Text>
 
              */}
-             <div>
-                <input type="text" placeholder="Search" className="searchbox"/><Button>Search</Button>
-             <Link to="/">
-             <RxHeart size="2rem"/></Link>
+      <Box>
+        <input type="text" placeholder="Search" className="searchbox" />
+        <Button>Search</Button>
+        <Link to="/">
+          <RxHeart size="2rem" />
+        </Link>
 
-             <Link to="/login"><IoPersonOutline  size="2rem"/></Link>
-            <Link to="/cart"> <IoBagHandleOutline   size="2rem"/></Link>
-
-
-            </div>
-
-
-
-        </div>
-        
-    )
-}
+        <Link to="/login">
+          <IoPersonOutline size="2rem" />
+        </Link>
+        <Link to="/cart">
+          {" "}
+          <IoBagHandleOutline size="2rem" />
+        </Link>
+      </Box>
+    </Box>
+  );
+};
 export default TopNavbar;
